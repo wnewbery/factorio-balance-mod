331e91dfbcfb3f2109ac2f8f5acfae34243f7b0a
William Newbery <wnewbery@hotmail.co.uk>
2016-04-22 18:15:42 +0100

    Changed recipes for fast and express underground transport belts

---
+++ data.lua
@@ -0,0 +1,13 @@
+
+
+local fastBelt = data.raw["recipe"]["fast-transport-belt-to-ground"]
+fastBelt.ingredients = {
+  {"fast-transport-belt", 5},
+  {"iron-plate", 10}
+}
+
+local expressBelt = data.raw["recipe"]["express-transport-belt-to-ground"]
+expressBelt.ingredients = {
+  {"express-transport-belt", 5},
+  {"iron-plate", 10}
+}
